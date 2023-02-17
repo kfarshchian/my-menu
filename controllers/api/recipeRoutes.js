e3af5fbce94ca57370fcdf403d77276e0bf349ab
Kameron <kfarshchian@gmail.com>
2023-02-17 14:40:07 -0700

    fix

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -8,13 +8,12 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: process.env.DB_USER,
-    // MySQL password
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
   },
-  console.log(`Connected to the books_db database.`)
+  console.log(`Connected to the diet_db database.`)
 ).promise();
 
 router.post('/create_seeds', async (req, res) => {
