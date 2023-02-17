085a3e89f1f4a83d0698bb37a470a484689ec2f6
Kameron <kfarshchian@gmail.com>
2023-02-17 13:49:19 -0700

    fix

--- config/dbInit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'logging into db root as techblog'
-mysql -u root --password="Buckingham1429"  << EOF
-SOURCE db/schema.sql;
-quit
-EOF
-echo 'Seeding Database'
-node seeds/seed.js
